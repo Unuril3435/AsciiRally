1fa277cd04e936f722ae21f8c9223359edda8dc7
Unuril <santiago.serranoesteve@gmail.com>
2025-11-06 15:27:36 +0100

    Testing networking

--- testClient.java
+++ testClient.java
@@ -1,12 +1,33 @@
+import java.io.PrintWriter;
 import java.net.*;
 import java.util.Scanner;
 
 public class testClient{
+
+    private Socket s;
+    private Scanner in;
+    private PrintWriter out;
+
+    public testClient() throws Exception{
+        this.s = new Socket("localhost", testServer.PORT);
+        this.in = new Scanner(this.s.getInputStream());
+        this.out = new PrintWriter(this.s.getOutputStream());
+    }
+
+    public String recieve(){
+        return this.in.nextLine();
+    }
+
+    public void send(String s){
+        this.out.println(s);
+        this.out.flush();
+    }
     public static void main(String[] args) throws Exception{
-        Socket s = new Socket("localhost", 1729);
+        testClient test = new testClient();
         System.out.println("Connection created");
-        Scanner in = new Scanner(s.getInputStream());
-        System.out.println(in.nextLine());
-        System.out.println(in.nextLine());
+        
+        System.out.println(test.recieve());
+        test.send("Hello World");
+        System.out.println(test.recieve());
     }
 }
--- testServer.java
+++ testServer.java
@@ -1,41 +1,52 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class testServer{
-    public static final int INITIAL_PORT = 1729;
-    private ServerSocket[] ss;
-    private Socket s;
-    private int currentPort;
+    public static final int PORT = 1729;
+    private ServerSocket ss;
+    private Socket[] s;
+    private Scanner[] in;
+    private PrintWriter[] out; 
 
     public testServer() throws Exception{
-        this.ss = new ServerSocket[1];
-        this.ss[0] = new ServerSocket(INITIAL_PORT);
-        this.currentPort = INITIAL_PORT;
+        this.ss = new ServerSocket(PORT);
+        this.s = new Socket[1];
+        this.in = new Scanner[1];
+        this.out = new PrintWriter[1];
     }
-    public void addSS(){addSS(1729);}
-    public void addSS(int port){
-        ServerSocket[] ans = new ServerSocket[this.ss.length+1];
-        for(int i = 0; i<this.ss.length; i++){
-            ans[i]=this.ss[i];
-        }
-        try{
-            ans[this.ss.length]= new ServerSocket(port);
-            System.out.println("Socket created in port " + port);
-            this.ss = ans;
-            this.s = this.ss[0].accept();
-            PrintWriter out = new PrintWriter(this.s.getOutputStream());
-            out.print("Hello World");
-            out.close();
-        }catch (IOException e){
-            this.currentPort++;
-            addSS(currentPort);
-        }
-    }
-    public static void main(String[] args) throws Exception{
-        testServer t = new testServer();
-        t.addSS();
 
+    public static <T> T[] add(T[] array, T item) {
+        T[] arr = Arrays.copyOf(array, array.length + 1);
+        arr[arr.length-1] = item;
+        return arr;
+    }
+
+    public void accept() throws Exception{
+        this.s = add(this.s, this.ss.accept());
+        this.out = add(this.out, new PrintWriter(this.s[this.s.length-1].getOutputStream()));
+        this.in = add(this.in, new Scanner(this.s[this.s.length-1].getInputStream()));
+    }
+    public void send(String s, int id) throws Exception{
+        this.out[id].println(s);
+        this.out[id].flush();
+    }
+    public String recieve(int id){
+        return this.in[id].nextLine();
+    }
+    
+    public static void main(String[] args) throws Exception{
+        testServer test = new testServer();
+
+        test.accept();
+        /*test.accept();
+        test.send("Hello",1);*/
+        test.send("bye",1);
+
+        System.out.println(test.recieve(1));
+        test.send("Something",1);
     }
 
     
